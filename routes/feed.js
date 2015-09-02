651d2cb928df9f32289ccdd0e078fcec52edfaa3
Esen Espinosa <sujumayas@gmail.com>
2015-09-02 16:16:25 -0500

    Update feed.js

--- routes/feed.js
+++ routes/feed.js
@@ -21,7 +21,7 @@ router.get('/:channel_name', function(req, res, next) {
 
   				slack.api('channels.history', {'channel':channel.id,'count':process.env.HISTORY_LENGTH} ,function(err, response){
 			  		for(var i = 0; i < response.messages.length; i++) {
-			  			if(response.messages[i].attachments && response.messages[i].subtype != "bot_message") {  				
+			  			if(response.messages[i].attachments) {  				
 				  			for(var j = 0; j < response.messages[i].attachments.length; j++) {
 				  				if(response.messages[i].attachments[j].title) {
 				  					var link = response.messages[i].attachments[j];
